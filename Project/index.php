567f145775545b87a1cb97261b5bfecf6921d910
jabulanitime8 <pirawit07@gmail.com>
2025-06-29 15:31:26 +0700

    Update index.php

--- Project/index.php
+++ Project/index.php
@@ -64,28 +64,15 @@ $current_username = $_SESSION['username'];
         <main>
             <h1>Dashboard</h1>
 
-            <div class="date">
-                <input type="date">
-            </div>
-
             <div class="insights">
                 <div class="sales">
-                    <span class="material-icons-sharp">analytics</span>
+                    <span class="material-icons-sharp">person</span>
                     <div class="middle">
                         <div class="left">
-                            <h3>Total Sales</h3>
-                            <h1>$25,000</h1>
-                        </div>
-                        <div class="progress">
-                            <svg>
-                                <circle cx='38' cy='38' r='36'></circle>
-                            </svg>
-                            <div class="number">
-                                <p>81%</p>
-                            </div>
+                            <h3>ผู้ใช้งานปัจจุบัน</h3>
+                            <h1><?php echo htmlspecialchars($current_username); ?></h1>
                         </div>
                     </div>
-                    <small class="text-muted">Last 24 Hour.</small>
                 </div>
                 <!-- End of sales -->
                 <div class="expenses">
